c93d1c719be18ef20353bc0283b49829413021fe
Pablo Zaved <pablo.azevedo@sou.inteli.edu.br>
2025-10-05 12:43:10 -0300

    Update branding and config for The Simple Split docs
    Updated Docusaurus configuration to reflect new project name 'The Simple Split', changed organization and repository references, updated favicon and logo, and adjusted navbar/footer links. Removed unused team member images and added new logo image. Also updated the TSF_dark.svg to a new design.

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,95 +1,84 @@
 // @ts-check
-// `@type` JSDoc annotations allow editor autocompletion and type checking
-// (when paired with `@ts-check`).
-// There are various equivalent ways to declare your Docusaurus config.
-// See: https://docusaurus.io/docs/api/docusaurus-config
-
-import { themes as prismThemes } from "prism-react-renderer";
-
-// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "TSF Documentation",
-  tagline: "Dinosaurs are cool",
-  favicon: "img/TSF.svg",
+  title: 'The Simple Split',
+  tagline: 'Dinosaurs are cool',
+  favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
-  url: "https://miguelclaret.github.io",
-  baseUrl: '/TheSimpleFund/',
+  url: 'https://zzaved.github.io', // Atualize conforme sua organização
+  baseUrl: '/The_Simple_Split/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: "miguelclaret", // Usually your GitHub org/user name.
-  projectName: "TheSimpleFund", // Usually your repo name.
+  organizationName: 'Zzaved', // Atualize conforme sua organização
+  projectName: 'The_Simple_Split',
 
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
+    defaultLocale: 'en',
+    locales: ['en'],
   },
 
   presets: [
     [
-      "classic",
+      'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: "./sidebars.js",
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
+          sidebarPath: './sidebars.js',
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-          routeBasePath: "/",
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          routeBasePath: '/',
         },
         blog: false,
         theme: {
-          customCss: "./src/css/custom.css",
+          customCss: './src/css/custom.css',
         },
       }),
     ],
   ],
-
+  
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      // Replace with your project's social card
-      image: "img/TSF.svg",
+      image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: "The Simple Fund Documentation",
+        title: 'The Simple Split',
         logo: {
-          alt: "The Simple Fund logo",
-          srcDark: "img/TSF.svg",
-          src: "img/TSF_dark.svg",
-
+          alt: 'The Simple Split',
+          src: '../static/img/TSS.png',
         },
         items: [
           {
-            href: "https://github.com/MiguelClaret/Teambalaie",
-            label: "GitHub",
-            position: "right",
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: 'Documentação',
+          },
+          {
+            href: 'https://github.com/Zzaved/The_Simple_Split', // Atualize para o URL correto do seu repositório
+            label: 'GitHub',
+            position: 'right',
           },
         ],
       },
       footer: {
-        style: "dark",
+        style: 'dark',
         links: [
           {
-            title: "More",
+            title: 'Docs',
             items: [
               {
-                label: "GitHub",
-                href: "https://github.com/MiguelClaret/Teambalaie",
+                label: 'Documentação',
+                to: '/',
               },
             ],
           },
+          // Resto do footer continua igual
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} The Simple Fund, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
       },
       prism: {
         theme: prismThemes.github,
